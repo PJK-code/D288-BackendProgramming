36e6eb1e52642a9023f734abe91af7e83eb2c3c6
Preston Kent <pkent1420@gmail.com>
2026-01-22 16:05:01 +0100

    Initial commit for Part B.

---
+++ demo/src/main/java/com/example/demo/PjkD288Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjkD288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjkD288Application.class, args);
+	}
+
+}
